072cbe359012e4f7ca8d43b74d99761f7ba24a2a
boudeffa <boudeffa@gmail.com>
2012-01-29 18:04:36 +0100

    - Les logos en png + Un peu de bricolage.

---
+++ developpement/V0.2/projets/derniers.php
@@ -0,0 +1,34 @@
+<?
+session_start();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+<title>Derniers projets</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+<?
+include('../scriptes.php');
+?>
+
+</head>
+
+<body id="sky">
+<?
+include('../menu.php');
+?>
+<div class="c9d-tabcnt">
+</br>
+</br>
+<div id="consultprojectContainer">
+
+<h1>Derniers Projets</h1>
+
+</div>
+<?
+include('explore.php');
+?>
+</div>
+</body>
+</html>
