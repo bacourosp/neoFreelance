8f39d2015534d195489c90ddd2ce82657d2b0c2f
boudeffa <boudeffa@gmail.com>
2012-02-02 23:50:37 +0100

    - Petites fonctionnalités supplémentaires pour ne pas se sentir seul.

---
+++ developpement/V0.2/chat/envoyer.php
@@ -0,0 +1,38 @@
+<?php
+//=========================================
+
+// information pour la connection à le DB
+
+//=========================================
+
+$host = 'mysql51-46.perso';
+$user = 'boudeffacowo';
+$pass = 'zoOPwOb8';
+$db = 'boudeffacowo';
+
+//=========================================    
+
+// connection à la DB
+
+//=========================================
+
+$link = mysql_connect ($host,$user,$pass) or die ('Erreur : '.mysql_error() );
+mysql_select_db($db) or die ('Erreur :'.mysql_error());
+
+try
+{
+    $message = $_POST["message"];
+	$date = date('c');
+    // Insertion du message à l'aide d'une requête préparée
+    $requete = "INSERT INTO CHAT VALUES('','".$date."','".$message."');";
+    mysql_query($requete);
+    // Redirection du visiteur vers la page du minichat
+    echo '<SCRIPT LANGUAGE="JavaScript">';
+    echo 'document.location.href="../index.php"'; /* vous pouvez aussi mettre http://www.monsite.com */
+    echo '</SCRIPT>';
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+}
+?>
