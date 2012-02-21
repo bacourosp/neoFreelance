977b9c396cf133ee16f7b0250237eba46892e6ea
boudeffa <boudeffa@gmail.com>
2012-02-21 10:15:41 +0100

    - sous-menu accéssible par login ?

---
+++ V0.2/accueil/sous-menu.php
@@ -0,0 +1,9 @@
+<div id="sous-menu-header" class="grid_12">
+<div>
+					
+			<ul class="sous-menu">
+            	<li><a href="/parametres/compte.php">Paramètres</a></li>
+
+			</ul>
+</div>
+</div>
---
+++ V0.2/projets/sous-menu.php
@@ -0,0 +1,13 @@
+<div id="sous-menu-header" class="grid_12">
+<div>
+					
+			<ul class="sous-menu">
+            	<li>
+                	<a href="/clients/creer.php"><span><b>Nouveau Projet</b></span></a>
+                </li>
+				<li>
+                	<a href="#"><span>Faire une offre</span></a>
+                </li>
+			</ul>
+</div>
+</div>
